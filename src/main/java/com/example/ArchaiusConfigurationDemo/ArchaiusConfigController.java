8665bd94934a6fcaae5bd201d3af8936174a65bf
Madan Dale <madan.dale@kpit.com>
2019-10-09 17:30:23 +0530

    mutliple property reading

--- src/main/java/com/example/ArchaiusConfigurationDemo/ArchaiusConfigController.java
+++ src/main/java/com/example/ArchaiusConfigurationDemo/ArchaiusConfigController.java
@@ -3,6 +3,7 @@ package com.example.ArchaiusConfigurationDemo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.config.DynamicLongProperty;
 import com.netflix.config.DynamicPropertyFactory;
 import com.netflix.config.DynamicStringProperty;
 
@@ -14,11 +15,16 @@ public class ArchaiusConfigController {
 	
 	private DynamicStringProperty propertyOneWithDynamic
     = DynamicPropertyFactory.getInstance()
+    .getStringProperty("spring.application.name", "not found!");
+  
+	private DynamicStringProperty propertyOneWithDynamic1
+    = DynamicPropertyFactory.getInstance()
     .getStringProperty("my.archaius.properties.one", "not found!");
-   
+	
+		  
   @GetMapping("/DynamicProperty")
   public String getPropertyValue() {
-  return propertyOneWithDynamic.getName() + ": " + propertyOneWithDynamic.get();
+  return propertyOneWithDynamic.getName() + ": " + propertyOneWithDynamic.get() +"\n seccond property : "+propertyOneWithDynamic1.getName() + ": " + propertyOneWithDynamic1.get();
   }
   
 }
